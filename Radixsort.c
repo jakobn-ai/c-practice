64d32d6849bef31f26860c54adbfaddce0d5ff7a
Jakob-Naucke <jakob.naucke@ibm.com>
2018-07-08 17:44:42 +0200

    Improved radix runtime

--- Radixsort.c
+++ Radixsort.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TESTLENGTH 30 
+#define TESTLENGTH 30
 #define TESTRANGE 100
 
-void count256(unsigned long *A, unsigned int *bytes, unsigned int len){
-    unsigned long sorted[len];
+void count256(unsigned long *A, unsigned int *bytes, unsigned long *sorted, unsigned int len){
     unsigned int frequency[256];
     unsigned int index[256];
     unsigned int i;
@@ -15,26 +14,27 @@ void count256(unsigned long *A, unsigned int *bytes, unsigned int len){
     index[0] = 0;
     for(i = 1; i < 256; i++) index[i] = index[i - 1] + frequency[i - 1];
     for(i = 0; i < len; i++) sorted[index[bytes[i]]++] = A[i];
-    for(i = 0; i < len; i++) A[i] = sorted[i];
 }
 
-void sort(unsigned long *A, unsigned int len){
+void sort(unsigned long *A, unsigned long *sorted, unsigned int len){
     unsigned int bytes[len];
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < len; j++) bytes[j] = ((A[j] >> (i*8)) & 255);
-        count256(A, bytes, len);
+        count256(A, bytes, sorted, len);
+        A = sorted;
     }
 }
 
 int main(){
     srand(time(0));
     unsigned long A[TESTLENGTH];
+    unsigned long sorted[TESTLENGTH];
     int i;
     for(i = 0; i < TESTLENGTH; i++){
         A[i] = rand() % TESTRANGE + 1;
         printf("%ld, ", A[i]);
     }
-    sort(A, TESTLENGTH);
+    sort(A, sorted, TESTLENGTH);
     printf("\n");
-    for(i = 0; i < TESTLENGTH; i++) printf("%ld, ", A[i]);
+    for(i = 0; i < TESTLENGTH; i++) printf("%ld, ", sorted[i]);
 }
